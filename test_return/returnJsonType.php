d839de3d916a5f8097f7bcf3d50c7259560f845a
2997215859 <2997215859@qq.com>
2016-03-21 00:06:01 +0800

    添加学习内容

---
+++ test_return/returnJsonType.php
@@ -0,0 +1,4 @@
+<?php 
+	$book = array("title"=>"天龙八部","intro"=>"人生八苦");
+	print_r(json_encode($book));
+?>
---
+++ test_return/returnXmlType.php
@@ -0,0 +1,5 @@
+<?php 
+header("Content-Type:text/xml");
+?>
+<?xml version="1.0" encoding="utf-8"?>
+<bookstore><book bid="b008"><title>天龙八部</title><intro>人生苦短</intro></book></bookstore>
